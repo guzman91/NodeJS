a1260a4e5d50b8af3fd3ed549f5c5faa7ecd6e11
guzman91 <guzmanukr91@gmail.com>
2020-11-04 13:07:40 +0200

    fs module commit

--- Refs/path_ref.js
+++ Refs/path_ref.js
@@ -1,4 +1,5 @@
 const path = require('path')
+const fs = require('fs')
 
 console.log('\nfilename: ',__filename);
 console.log('path.basename: ',path.basename(__filename));
@@ -9,3 +10,11 @@ console.log('path.parse: ',path.parse(__filename));
 
 console.log('path.join: ',path.join(__dirname, 'test','third.html'));
 console.log('path.resolve: ',path.resolve(__dirname, 'test','third.html'),'\n');
+
+
+fs.mkdir(path.join(__dirname,'Notes'), err => {
+if (err) throw new Error(err)
+
+console.log('Folder is created');
+})
+
